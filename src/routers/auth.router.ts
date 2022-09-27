f42d2a155d1a6bd9986af232314d1ab583218243
unknown <dodfood>
2022-09-27 16:14:39 +0700

    complate logi

---
+++ src/routers/auth.router.ts
@@ -0,0 +1,32 @@
+import express from 'express';
+import passport from 'passport';
+
+const router = express.Router();
+import wrapperError from '../containErr/err'
+
+import AuthController from "../controllers/auth.controller";
+
+import checkLogin from "../middleware/check.login";
+
+import authController from "../controllers/auth.controller";
+
+router.get('/', wrapperError(AuthController.login))
+
+router.post('/login', wrapperError(checkLogin.checkUserOrAdminRouter))
+
+router.get('/register', wrapperError(authController.register))
+
+router.post('/register', wrapperError(authController.register))
+
+router.get('/error', wrapperError(authController.error))
+
+router.get('/google', passport.authenticate('google', {scope: ['profile']}));
+
+router.get('/google/callback', passport.authenticate('google', {failureRedirect: '/'}),
+
+    async (req, res) => {
+
+        res.redirect('/auth/user')
+    }
+)
+export default router
